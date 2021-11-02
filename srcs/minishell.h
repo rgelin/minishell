4d52082e3cd2948869cf378038a3b4c45c00b06b
jvander- <jvander-@student.s19.be>
2021-11-02 08:45:16 +0100

    create folder srcs, utils. Create minishell.c and .h

---
+++ srcs/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvander- <jvander-@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/02 08:40:08 by jvander-          #+#    #+#             */
+/*   Updated: 2021/11/02 08:43:45 by jvander-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <unistd.h>
+
+#endif
