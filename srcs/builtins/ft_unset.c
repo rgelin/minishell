214c95a421599e1786e70eaaaa8247dc3a05bc98
Rémy Gelin <Rems@MacBook-Air-de-Remy.local>
2021-11-20 17:45:54 +0100

    unset

--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -9,7 +9,8 @@ void	ft_unset(char **cmd, char ***env)
 	arg = cmd[1];
 	if (!arg || !find_var_in_env(arg, *env))
 		return ;
-	new_env = (char **)malloc(sizeof(char *) * (ft_tabsize(*env)));
+	// new_env = (char **)malloc(sizeof(char *) * (ft_tabsize(*env) + 1));
+	new_env = ft_calloc(sizeof(char *), (ft_tabsize(*env) + 1));
 	if (!new_env)
 		exit(EXIT_FAILURE);
 	i = -1;
