94edf5a9322d98c1f62e2b2de99e319b4b7b3b4a
jvander- <jvander-@student.s19.be>
2021-11-22 14:54:56 +0100

    fix create command

--- srcs/ft_exec.c
+++ srcs/ft_exec.c
@@ -1,29 +1,5 @@
 #include "minishell.h"
 
-static char	**create_cmd(t_exc command)
-{
-	char	**to_ret;
-	int		i;
-
-	i = 0;
-	to_ret = (char **)malloc(sizeof(char *) * 4);
-	if (!to_ret)
-		return (NULL);
-	if (command.cmd)
-		to_ret[i++] = command.cmd;
-	if (command.opt)
-		to_ret[i++] = command.opt;
-	//if (command.arg)
-	//	to_ret[i++] = command.arg;
-	to_ret[i] = NULL;
-	while (--i >= 0)
-	{
-		if (!to_ret[i])
-			return (NULL);
-	}
-	return (to_ret);
-}
-
 int	ft_create_all_exec(char ***folder, t_exc command)
 {
 	int	i;
