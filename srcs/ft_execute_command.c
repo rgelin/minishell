37df400ca3931e57a161ea4aefd4e037c631961b
rgelin <rgelin@student.s19.be>
2021-12-06 14:27:17 +0100

    pull

--- srcs/ft_execute_command.c
+++ srcs/ft_execute_command.c
@@ -1,56 +1,34 @@
 
 #include "minishell.h"
 
-extern int g_exit_code;
-
-
-// int	check_builtin(char *cmd)
-// {
-// 	if (!ft_strncmp(cmd, "echo", 4))
-// 		return (ECHO);
-// 	if (!ft_strncmp(cmd, "cd", 2))
-// 		return (CD);
-// 	if (!ft_strncmp(cmd, "pwd", 3))
-// 		return (PWD);
-// 	if (!ft_strncmp(cmd, "export", 6))
-// 		return (EXPORT);
-// 	if (!ft_strncmp(cmd, "unset", 5))
-// 		return (UNSET);
-// 	if (!ft_strncmp(cmd, "env", 3))
-// 		return (ENV);
-// 	if (!ft_strncmp(cmd, "exit", 4))
-// 		return (EXIT);
-// 	return (0);
-// }
-
 int	ft_execute_command(t_exc exc, char ***env)
 {
-	if (!ft_strncmp(exc.cmd, "echo", 5))
+	if (check_builtin(exc.cmd) == ECHO)
 	{
 		ft_echo(exc);
 		return (ECHO);
 	}
-	if (!ft_strncmp(exc.cmd, "cd", 3))
+	if (check_builtin(exc.cmd) == CD)
 	{
 		ft_cd(exc, env);
 		return (CD);
 	}
-	if (!ft_strncmp(exc.cmd, "pwd", 4))
+	if (check_builtin(exc.cmd) == PWD)
 	{
 		ft_pwd();
 		return (PWD);
 	}
-	if (!ft_strncmp(exc.cmd, "export", 7))
+	if (check_builtin(exc.cmd) == EXPORT)
 	{
 		ft_export(exc, env);
 		return (EXPORT);
 	}
-	if (!ft_strncmp(exc.cmd, "unset", 6))
+	if (check_builtin(exc.cmd) == UNSET)
 	{
 		ft_unset(exc, env);
 		return (UNSET);
 	}
-	if (!ft_strncmp(exc.cmd, "env", 3))
+	if (check_builtin(exc.cmd) == ENV)
 	{
 		ft_env((*env));
 		return (ENV);
