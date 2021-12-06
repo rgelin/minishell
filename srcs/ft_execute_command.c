009c093922706bef0bb86182f265d52d13b060fc
rgelin <rgelin@student.s19.be>
2021-12-06 16:06:54 +0100

    pull

--- srcs/ft_execute_command.c
+++ srcs/ft_execute_command.c
@@ -3,26 +3,6 @@
 
 extern int g_exit_code;
 
-
-// int	check_builtin(char *cmd)
-// {
-// 	if (!ft_strncmp(cmd, "echo", 4))
-// 		return (ECHO);
-// 	if (!ft_strncmp(cmd, "cd", 2))
-// 		return (CD);
-// 	if (!ft_strncmp(cmd, "pwd", 3))
-// 		return (PWD);
-// 	if (!ft_strncmp(cmd, "export", 6))
-// 		return (EXPORT);
-// 	if (!ft_strncmp(cmd, "unset", 5))
-// 		return (UNSET);
-// 	if (!ft_strncmp(cmd, "env", 3))
-// 		return (ENV);
-// 	if (!ft_strncmp(cmd, "exit", 4))
-// 		return (EXIT);
-// 	return (0);
-// }
-
 int	ft_execute_command(t_exc exc, char ***env)
 {
 	if (!ft_strncmp(exc.cmd, "echo", 5))
