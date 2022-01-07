01462bee9f5d846c64000cec15b4963106593f26
jlong <jlong@student.s19.be>
2022-01-07 10:32:31 +0100

    add_new_parsing

---
+++ parsing/new_parsing.c
@@ -0,0 +1,53 @@
+#include "../srcs/minishell.h"
+
+t_pars new_parsing(char *line)
+{
+	t_pars	tab;
+	char	**tmp;
+	int		i;
+	int		heredoc;
+	int		previous;
+	int		cmd;
+
+	i = 0;
+	heredoc = 0;
+	previous = 0;
+	cmd = 0;
+	init_tab(&tab);
+	tmp = ft_split_parsing(line, ' ');
+	while (tmp[i] != NULL)
+	{
+		if (tmp[i][0] == '<' && tmp[i][1] == '<')
+		{
+			//fonction heredoc
+			//heredoc = 1 si le << est collé au texte je vais devoir changer
+			//le heredoc à 0 est bien mettre les lettres à rentré
+			// et noté previous a 1
+		}
+		else if (*tmp[i] == '<'|| *tmp[i] == '>')
+		{
+			//fonction redirection
+			//si heredoc est = 1 c'est une error ? a verif
+		}
+		else if (*tmp && heredoc == 1)
+		{
+			//c'est ce qu on doit rentrer dans le heredoc
+			//fonction heredoc
+			//heredoc = 0
+			//previous a 1
+		}
+		else if (*tmp && previous == 1)
+		{
+			//ca devient ma commande
+			//cmd = 1
+			//previous = 0
+		}
+		else if (*tmp && previous == 0 && cmd == 1)
+		{
+			//fonction argument ou option ?
+			//si il y a un - c'est une option
+			//sinon c'est un argument
+		}
+		i++;
+	}
+}
