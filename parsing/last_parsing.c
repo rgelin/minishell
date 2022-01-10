b4424ffce1923e10786de9bfe3f34b7bcb6eda58
jlong <jlong@student.s19.be>
2022-01-10 19:53:27 +0100

    add_clean_redirection

--- parsing/last_parsing.c
+++ parsing/last_parsing.c
@@ -57,6 +57,46 @@ char	*ft_command(char *command)
 	return (new);
 }
 
+char	*new_redirect(char *line)
+{
+	int		i;
+	t_tmp	tmp;
+
+	init_tmp(&tmp);
+	i = 0;
+	while (line[i] == '>' || line[i] == '<')
+		i++;
+	tmp.tmp = ft_substr(line, 0, i );
+	while(line[i] && line[i] == ' ')
+		i++;
+	tmp.n = i;
+	while (line[i] && ft_isascii(line[i]) && line[i] != ' ')
+		i++;
+	tmp.var = ft_substr(line, tmp.n, i - tmp.n);
+	tmp.new_line = ft_strjoin_double_free(tmp.tmp, tmp.var);
+	free(line);
+	line = NULL;
+	//free(tmp.tmp);
+	//free(tmp.var);
+	return (tmp.new_line);
+}
+
+char	**get_redirect_bis(char **tab_redirect)
+{
+	int		i;
+	char	**new;
+
+	new = NULL;
+	i = 0;
+	while (tab_redirect[i])
+	{
+		tab_redirect[i] = new_redirect(tab_redirect[i]);
+		i++;
+	}
+	new = tab_redirect;
+	return (new);
+}
+
 t_exc	*last_parsing(t_pars *tab, char **env)
 {
 	int		i;
@@ -75,7 +115,7 @@ t_exc	*last_parsing(t_pars *tab, char **env)
 		if (tab[i].option)
 			last_tab[i].opt = option(tab[i].option);
 		if (tab[i].redirect)
-			last_tab[i].redirect = tab[i].redirect;
+			last_tab[i].redirect = get_redirect_bis(tab[i].redirect);
 		if (tab[i].heredoc)
 			last_tab[i].heredoc = tab[i].heredoc;
 		if (tab[i].arg)
