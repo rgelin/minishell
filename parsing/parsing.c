643a7cd6769eaa079fe65248a7279e3592bcd6e3
jlong <jlong@student.s19.be>
2022-01-20 10:07:35 +0100

    error_synthaxe

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlong <jlong@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jlong <jlong@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 12:21:29 by jlong             #+#    #+#             */
-/*   Updated: 2022/01/17 18:05:12 by jlong            ###   ########.fr       */
+/*   Updated: 2022/01/20 10:06:08 by jlong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,75 @@ void	init_quote(t_quote *quote)
 	quote->double_quote = 0;
 }
 
+int	check_error_synthax(t_state *s)
+{
+	char	**tmp;
+	int		i;
+	int		error;
+
+	i = -1;
+	error = 0;
+	tmp = ft_split_parsing(s->line, ' ');
+	while (tmp && tmp[++i] != NULL)
+	{
+		if (tmp[i] && ft_strncmp(tmp[i], ">>", 2) && tmp[i + 1] == NULL)
+			error = 1;
+		else if  (tmp[i] && ft_strncmp(tmp[i], "<<", 2) && tmp[i + 1] == NULL)
+			error = 1;
+		else if  (tmp[i] && ft_strncmp(tmp[i], "<", 1) && tmp[i + 1] == NULL)
+			error = 1;
+		else if  (tmp[i] && ft_strncmp(tmp[i], ">", 1) && tmp[i + 1] == NULL)
+			error = 1;
+		if (error == 1)
+			break ;
+	}
+	ft_free(tmp, ft_tabsize(tmp));
+	if (error == 1)
+		return (0);
+	return (1);
+}
+
+int	check_error_synthax_bis(t_state *s)
+{
+	char	**tmp;
+	int		i;
+	int		error;
+
+	i = -1;
+	error = 0;
+	tmp = ft_split_parsing(s->line, ' ');
+	while (tmp && tmp[++i] != NULL)
+	{
+		if (tmp[i] && ft_strncmp(tmp[i], ">>>", 2))
+			error = 1;
+		if (tmp[i] && ft_strncmp(tmp[i], "<<<", 2))
+			error = 1;
+		if (tmp[i] && ft_strncmp(tmp[i], "||", 1))
+			error = 1;
+		if (error == 1)
+			break ;
+	}
+	ft_free(tmp, ft_tabsize(tmp));
+	if (error == 1)
+		return (0);
+	return (1);
+}
+//verifier les return 0
 int	check_parsing(t_state *s)
 {
-	if (s->n_of_pipe == -1)
+	if (!check_quote(s->line, s->eof))
+	{
+		write(2, "Error quote\n", 12);
+		return (0);
+	}
+	if ((!check_error_synthax_bis(s)) || (!check_error_synthax(s)))
 	{
 		ft_putendl_fd("Error syntax", 2);
 		return (0);
 	}
-	if (!check_quote(s->line, s->eof))
+	if (s->n_of_pipe == -1)
 	{
-		write(2, "Error quote\n", 12);
+		ft_putendl_fd("Error syntax", 2);
 		return (0);
 	}
 	if (s->n_of_pipe >= 0)
